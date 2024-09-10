c679bc09e55b1dd78e1f2b918ba5ff823b860f84
Alex Herken <alexxherken2@gmail.com>
2024-09-09 22:23:03 -0300

    classe1

--- src/model_entities/Reserva.java
+++ src/model_entities/Reserva.java
@@ -1,12 +1,16 @@
 package model_entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reserva {
 
     private Integer numero;
     private Date dataEntrada;
     private Date dataSaida;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reserva() {
     }
@@ -33,6 +37,28 @@ public class Reserva {
         return dataSaida;
     }
 
+    public long duracao() {
+        long diferenca = dataSaida.getTime()  - dataEntrada.getTime();
 
+        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS); // converte milissegundos em dias
+    }
+
+    public void atualizaData(Date dataEntrada, Date dataSaida){
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Quarto "
+                + numero
+                + ", Entrada: "
+                + getDataEntrada()
+                + "Saida: "
+                + sdf.format(dataSaida)
+                + ", "
+                + duracao()
+                + " noites");
+    }
 
 }
